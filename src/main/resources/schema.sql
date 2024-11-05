8a0d9b323ba4bf8baf81085e2972b046c54349a2
FerosC101 <vincevillar02@gmail.com>
2024-11-06 00:44:07 +0800

    updated schema

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,3 +1,11 @@
+CREATE DATABASE `wandat`;
+
+CREATE TABLE users (
+    user_id BIGINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(50) UNIQUE NOT NULL,
+    password VARCHAR(100) NOT NULL
+);
+
 CREATE TABLE credentials (
     credential_id INT AUTO_INCREMENT PRIMARY KEY,
     user_id BIGINT UNSIGNED NOT NULL,
@@ -18,27 +26,26 @@ CREATE TABLE user_details (
     age INT,
     height FLOAT,
     weight FLOAT,
+    gender VARCHAR(15),
+    profession VARCHAR(50),
     FOREIGN KEY (user_id) REFERENCES users(user_id) ON DELETE CASCADE
 );
 
-ALTER TABLE user_details
-ADD COLUMN gender VARCHAR(15);
-
 CREATE TABLE user_questions (
-                                question_id INT AUTO_INCREMENT PRIMARY KEY,
-                                question_text VARCHAR(255),
-                                question_type ENUM('scale', 'multiple_choice'),
-                                max_scale INT,
-                                choice_options VARCHAR(255)
+    question_id INT AUTO_INCREMENT PRIMARY KEY,
+    question_text VARCHAR(255),
+    question_type ENUM('scale', 'multiple_choice'),
+    max_scale INT,
+    choice_options VARCHAR(255)
 );
 
 CREATE TABLE user_answers (
-                              answer_id INT AUTO_INCREMENT PRIMARY KEY,
-                              user_id BIGINT,
-                              question_id INT,
-                              answer INT,
-                              exp_points INT,
-                              FOREIGN KEY (user_id) REFERENCES users(user_id) ON DELETE CASCADE,
-                              FOREIGN KEY (question_id) REFERENCES user_questions(question_id) ON DELETE CASCADE
+    answer_id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id BIGINT,
+    question_id INT,
+    answer INT,
+    exp_points INT,
+    FOREIGN KEY (user_id) REFERENCES users(user_id) ON DELETE CASCADE,
+    FOREIGN KEY (question_id) REFERENCES user_questions(question_id) ON DELETE CASCADE
 );
 
