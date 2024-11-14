0226342fca878591132cb0b3116e6d91144c6663
FerosC101 <vincevillar02@gmail.com>
2024-11-14 20:24:17 +0800

    Proper File structuring

--- src/main/java/com/example/connection/UserDAO.java
+++ src/main/java/com/example/connection/UserDAO.java
@@ -3,10 +3,7 @@ package com.example.connection;
 import com.example.model.User;
 
 import java.sql.*;
-import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+
 
 
 import static com.example.connection.DBConnection.getConnection;
@@ -45,22 +42,6 @@ public class UserDAO {
         user.setGender(rs.getString("gender"));
         user.setProfession(rs.getString("profession"));
         return user;
-    }
-
-    public int getUserExp(int userId) throws SQLException {
-        String query = "SELECT exp FROM users WHERE user_id = ?";
-        int exp = 0;
-
-        try (Connection conn = getConnection();
-             PreparedStatement pstmt = conn.prepareStatement(query)) {
-            pstmt.setInt(1, userId);
-            ResultSet rs = pstmt.executeQuery();
-
-            if (rs.next()) {
-                exp = rs.getInt("exp");
-            }
-        }
-        return exp;
     }
 
     public void updateUserExp(int userId, int additionalExp) throws SQLException {
